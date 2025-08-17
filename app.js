0ae257de04d6f4ca2ca5c62c93501c110e9a0880
Joyce-Cristina <joyce.silva461@etec.sp.gov.br>
2025-08-17 02:25:51 -0300

    ci funcionando

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
+// app.js
 const express = require("express");
-const connection = require("./Tcc biblion/java/database");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
-app.use(express.json());
 
-// exemplo de rota de teste
+// Middlewares globais
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Importa rotas
+const usuarioRoutes = require("./routes/usuario");
+const funcionarioRoutes = require("./routes/funcionario");
+const livroRoutes = require("./routes/livro");
+const generoRoutes = require("./routes/genero");
+
+// Usa rotas
+app.use("/usuario", usuarioRoutes);
+app.use("/funcionario", funcionarioRoutes);
+app.use("/livro", livroRoutes);
+app.use("/genero", generoRoutes);
+
+// Rota de teste
 app.get("/", (req, res) => {
   res.send("Servidor rodando 🚀");
 });
