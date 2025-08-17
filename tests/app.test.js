9050427996aa09b9eb745dec34d4dfbc0d9d8c1f
Joyce-Cristina <joyce.silva461@etec.sp.gov.br>
2025-08-17 02:30:56 -0300

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -9,15 +9,17 @@ let conn;
 let userId;
 
 beforeAll(async () => {
-  pool = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "bibliontec",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-  });
+  const pool = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "bibliontec",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  charset: "utf8mb4" // adiciona isso
+});
+
 
   conn = await pool.getConnection();
 
