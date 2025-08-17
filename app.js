31e521a7b9e8f353e6773d32a5fb90fd4f3c862e
Joyce-Cristina <joyce.silva461@etec.sp.gov.br>
2025-08-17 02:27:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,17 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Importa rotas
-const usuarioRoutes = require("./routes/usuario");
-const funcionarioRoutes = require("./routes/funcionario");
-const livroRoutes = require("./routes/livro");
-const generoRoutes = require("./routes/genero");
-
-// Usa rotas
-app.use("/usuario", usuarioRoutes);
-app.use("/funcionario", funcionarioRoutes);
-app.use("/livro", livroRoutes);
-app.use("/genero", generoRoutes);
 
 // Rota de teste
 app.get("/", (req, res) => {
